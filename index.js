0a0b2059178fbb860c023933561c13d752b22858
AHMED AFANDY <eng.computer1@yahoo.com>
2020-02-17 02:31:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ function nestedTarget()
   return document.getElementById('nested').querySelector('div.target')
   
 }
+<<<<<<< HEAD
 
 function increaseRankBy(n)
 {
@@ -17,6 +18,14 @@ function increaseRankBy(n)
   .querySelectorAll('ul.ranked-list li');
   
   for (let i = 0; i < rank.length; i++)
+=======
+function increaseRankBy(n)
+{
+  cons rank = document.getElementById('app')
+  .querySelectorAll('ul.ranked-list li');
+  
+  for (let i = 0; i < lis.length; i++)
+>>>>>>> 5a76c8149c69a95d14f6b2f6948d464b929f5266
   {
   rank[i].innerHTML =parseInt(rank[i].innerHTML)+n;
   }
@@ -24,6 +33,7 @@ function increaseRankBy(n)
 }
  function deepestChild()
  {
+<<<<<<< HEAD
    var  deep= document.getElementById('grand-node').querySelectorAll('div')
    var  deepest =deep[deep.length-1]
    return deepest
@@ -32,5 +42,13 @@ function increaseRankBy(n)
        //  var deepest=deep[i]
        //}
       //return deepest
+=======
+   var doc= document.getElementById('grand-node')
+    for(var i=0;i<doc.length;i++)
+    {
+      doc.querySelector(i)
+    }
+   return doc.innerHTML
+>>>>>>> 5a76c8149c69a95d14f6b2f6948d464b929f5266
    
  }
